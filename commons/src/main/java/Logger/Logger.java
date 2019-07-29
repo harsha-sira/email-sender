6e7b52769a6b23eb0576b7915b0fb33d26f4c093
Harsha Siriwardhana <harshasandamal100@gmail.com>
2019-07-30 00:53:12 +0530

    DEV:Email-Sender: adding custom logger

---
+++ commons/src/main/java/Logger/Logger.java
@@ -0,0 +1,52 @@
+package Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.*;
+
+/**
+ * Created by Harsha Siriwardhana on 7/30/2019.
+ */
+public class Logger {
+
+    private static boolean initiated = false;
+    private static final java.util.logging.Logger LOGGER = java.util.logging.Logger.getLogger(java.util.logging.Logger.GLOBAL_LOGGER_NAME);
+
+    public static java.util.logging.Logger getLogger() {
+        if (!initiated) {
+            setup();
+            initiated = true;
+        }
+        return LOGGER;
+    }
+
+    public static void setup() {
+        LogManager.getLogManager().reset();
+        LOGGER.setLevel(Level.ALL);
+
+        //console handler
+        ConsoleHandler ch = new ConsoleHandler();
+        ch.setLevel(Level.ALL);
+        LOGGER.addHandler(ch);
+
+        //file handler
+        // check if logs dir exists
+        File logDir = new File("./logs/");
+        if( !(logDir.exists()) ){
+            logDir.mkdir();
+        }
+        FileHandler fh = null;
+        try {
+            fh = new FileHandler("logs/email-logger.log", true);
+            fh.setLevel(Level.SEVERE);
+            fh.setFormatter(new SimpleFormatter());
+            LOGGER.addHandler(fh);
+        } catch (IOException e) {
+            LOGGER.severe(e.getMessage());
+        }
+
+
+    }
+
+
+}
